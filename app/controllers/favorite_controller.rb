9bd2c11f7e2ffcd7680237e0bb4eb49ed4447b66
稲垣 洸雄 <lego.nori@gmail.com>
2013-04-23 02:01:25 +0900

    ボタンのrender化

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,5 +1,6 @@
 class FavoriteController < ApplicationController
   def create
+    return redirect_to '/404.html' unless request.xhr?
     post = Post.find(params[:post_id])
     Favorite.create(post_id: post.id, user_id: current_user.id)
   end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ActiveRecord::Base
   attr_accessible :post_id, :user_id
   belongs_to :post
+
+  scope :latest, -> { order("updated_at desc") }
 end
